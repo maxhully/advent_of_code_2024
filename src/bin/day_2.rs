1b49e34f4bcf149064d2436a9e7ff760d170f830
Max Hully <max.hully@gmail.com>
2024-12-03 23:26:09 -0500

    I'm proud of this even though it's not right

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -1,30 +1,60 @@
-fn is_safe_level(level: &Vec<i32>) -> bool {
-    let initial_ordering = level[0].cmp(&level[1]);
-    // Must be all either increasing or decreasing
-    if initial_ordering.is_eq() {
-        return false;
-    }
-    let pairs = level.iter().zip(level.iter().skip(1));
-    for (prev, next) in pairs {
-        let ordering = prev.cmp(next);
-        // The levels are either all increasing or all decreasing.
-        if ordering != initial_ordering {
-            return false;
-        }
-        let gap = prev.abs_diff(*next);
-        // Any two adjacent levels differ by at least one and at most three.
-        // (We already covered the "at least one" part with our ordering handling above)
-        if gap > 3 {
-            return false;
+use std::cmp::Ordering;
+
+struct LevelDiff {
+    ordering: Ordering,
+    gap: u32,
+}
+
+impl LevelDiff {
+    fn from_pair(prev: i32, next: i32) -> LevelDiff {
+        LevelDiff {
+            ordering: prev.cmp(&next),
+            gap: prev.abs_diff(next),
         }
     }
-    true
+}
+
+fn is_safe_level(levels: &Vec<i32>, bad_level_tolerance: usize) -> bool {
+    println!("\nlevels = {:?}", levels);
+    let diffs: Vec<LevelDiff> = levels
+        .iter()
+        .zip(levels.iter().skip(1))
+        .map(|(prev, next)| LevelDiff::from_pair(*prev, *next))
+        // Filter out the level diffs with too-large gaps right away
+        .filter(|diff| 1 <= diff.gap && diff.gap <= 3)
+        .collect();
+
+    println!("levels.len() = {}", levels.len());
+    // Now, see if we have enough good levels (at least the total - bad_level_tolerance)
+    // in either the increasing or decreasing direction.
+    let mut increasing_count: usize = 0;
+    let mut decreasing_count: usize = 0;
+    for gap in diffs.iter() {
+        match gap.ordering {
+            Ordering::Greater => {
+                increasing_count += 1;
+            }
+            Ordering::Less => {
+                decreasing_count += 1;
+            }
+            Ordering::Equal => (),
+        }
+    }
+    println!("increasing_count = {}", increasing_count);
+    println!("decreasing_count = {}", decreasing_count);
+
+    let target_count = levels.len() - 1 - bad_level_tolerance;
+    let is_safe = increasing_count >= target_count || decreasing_count >= target_count;
+    if !is_safe {
+        println!("not safe!!");
+    }
+    is_safe
 }
 
 fn day_2() {
     // Puzzle 1
     let contents = std::fs::read_to_string("inputs/day_2.txt").unwrap();
-    let levels: Vec<Vec<i32>> = contents
+    let reports: Vec<Vec<i32>> = contents
         .lines()
         .map(|line| {
             line.split_whitespace()
@@ -33,10 +63,20 @@ fn day_2() {
         })
         .collect();
 
-    let num_safe_levels: i32 = levels.iter().map(|level| is_safe_level(level) as i32).sum();
-    println!("num_safe_levels = {}", num_safe_levels);
+    // let num_safe_reports: i32 = reports
+    //     .iter()
+    //     .map(|level| is_safe_level(level, 0) as i32)
+    //     .sum();
+    // println!("num_safe_reports = {}", num_safe_reports);
 
     // Puzzle 2
+    println!("Engaging Problem Dampener!!");
+    let num_safe_reports: i32 = reports
+        .iter()
+        // .take(12)
+        .map(|level| is_safe_level(level, 1) as i32)
+        .sum();
+    println!("num_safe_reports = {}", num_safe_reports);
 }
 
 fn main() {
